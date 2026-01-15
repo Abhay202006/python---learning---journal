85e807a7ebc93ed8abf82e95b4977544d29f0b17
Abhay Sharma <abhay20062008@gmail.com>
2026-01-15 13:51:09 +0530

    Create variables.py
    Added variables basics with examples

---
+++ basics/variables.py
@@ -0,0 +1,41 @@
+# Definition
+
+
+# Varaiable is a memory space reserved in computer memory to store a value.
+# The value can be changed further if needed.
+# A variable can hold values of different data types.
+
+
+
+# Rules
+
+# 1. A variable name must start with a letter or the underscore (_) . Example - _XYZ, Var_name
+# 2. A variable name cannot start with a number
+# 3. Variables names are case - sensitive
+# 4. A variable name can only contain alpha numeric characters and underscores.
+# 5. The resreved words ( keywords ) cannot be used for naming the varaiable.
+
+
+
+# Code
+
+name = "Abhay"     #String
+age = 19           #Integer
+is_student = True  #Boolean
+marks = 9.8        #Float
+
+print("Name : ",name)
+print("Age : ",age)
+print("Student :",is_student)
+print("Marks : ",marks)
+
+
+
+#OR
+
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Student: {is_student}")
+print(f"Marks: {marks}")
+
