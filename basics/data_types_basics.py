297a3d2d663aded15e948527851427d10b6c9cfb
Abhay Sharma <abhay20062008@gmail.com>
2026-01-16 15:50:14 +0530

    Create data_types_basics.py
    Added data types examples in Python

---
+++ basics/data_types_basics.py
@@ -0,0 +1,16 @@
+# basics/data_types.py
+# Topic: Data Types in Python
+
+# type() tells the type of data the value is of
+
+# Code
+
+number = 10
+price = 99.5
+name = "Abhi"
+is_active = True
+
+print(type(number))
+print(type(price))
+print(type(name))
+print(type(is_active))
